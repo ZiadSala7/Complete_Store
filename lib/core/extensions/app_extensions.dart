13685527bb12cddde9cec365552320b439673500
ZiadSala7 <zslah2800@gmail.com>
2024-09-25 15:00:30 +0300

    extensions, colors, images for project

--- lib/core/extensions/app_extensions.dart
+++ lib/core/extensions/app_extensions.dart
@@ -1,9 +1,12 @@
 import 'package:complete_store/core/styles/themes/color_extensions.dart';
+import 'package:complete_store/core/styles/themes/my_assets_extensions.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
 extension on BuildContext {
   ColorExtensions get color => Theme.of(this).extension<ColorExtensions>()!;
+  MyAssetsExtensions get assets =>
+      Theme.of(this).extension<MyAssetsExtensions>()!;
   Future<Object?> pushRouter(String path) {
     return GoRouter.of(this).push(path);
   }

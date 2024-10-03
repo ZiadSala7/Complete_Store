a96bc6502f5c601d7696b5c359f75d6e686ede6f
ZiadSala7 <zslah2800@gmail.com>
2024-10-03 17:46:50 +0300

    login datasource and repo

--- lib/core/services/graphql/api_service.dart
+++ lib/core/services/graphql/api_service.dart
@@ -1,3 +1,5 @@
+import 'package:complete_store/features/auth/models/login/login_response.dart';
+import 'package:complete_store/features/auth/models/user_role/user_role_response.dart';
 import 'package:dio/dio.dart';
 import 'package:retrofit/retrofit.dart';
 
@@ -9,4 +11,11 @@ const String graphql = '/graphql';
 @RestApi(baseUrl: baseUrl)
 abstract class ApiService {
   factory ApiService(Dio dio, {String baseUrl}) = _ApiService;
+
+  @POST(graphql)
+  Future<LoginResponse> login(
+    @Body() Map<String, dynamic> mutation,
+  );
+  @GET('/api/v1/auth/profile')
+  Future<UserRoleResponse> userRole();
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,12 +5,14 @@
 import FlutterMacOS
 import Foundation
 
+import cloud_firestore
 import connectivity_plus
 import firebase_core
 import firebase_storage
 import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
   ConnectivityPlusPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlusPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseStoragePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseStoragePlugin"))
